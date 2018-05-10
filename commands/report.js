a04aa278233937f381554a3efba34991b54f42b2
Serenium <32240032+serenium09@users.noreply.github.com>
2018-05-10 16:35:23 +0700

    Delete report.js

--- commands/report.js
+++
@@ -1,9 +0,0 @@
-const Discord = ("discord.js");
-
-module.exports.run = async (bot, message, args) => {
-    console.log("Komando laporan berhasil.");
-}
-
-module.exports.help = {
-    name : "laporan"
-}
